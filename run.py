0be804c871460b59d56e9cf87798a5a0463a0e31
Pat <patrick.jacob@rocketmail.com>
2018-09-11 17:06:07 +1000

    Just a very basic sceleton for the project

---
+++ run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+* Authors : Dzmitry Kakaruk, Calvin Schnierer, Patrick Jacob
+*
+* Version info 1.0.
+*
+* This program is created for Assignment 2 of Programming Internet of Things -  Course Master of IT - RMIT University.
+* This code has parts which are inspired by the course material of  - Programming Internet of Things  - RMIT University.
+*
+* The purpose of the Program is to simulate a automatized Medical Office.
+* Booking of Consultations, Registration and Recording of Consultations Notes shall be managed via the MAPS Program
+* For more information please see: https://github.com/kokaruk/IOT-A1.
+*
+*
+* Copyright notice - All copyrights belong to  Dzmitry Kakaruk, Calvin Schnierer, Patrick Jacob
+"""
+
+from MAPS import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
