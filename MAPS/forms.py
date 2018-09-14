5902b3fb67b99227b12157b1feec692e715746ff
Pat <patrick.jacob@rocketmail.com>
2018-09-15 01:40:36 +1000

    Adjustments to the consultation form now has validations  and expanding the layout to have drop down menu for each User  and also the first implementation of  consultation appointment booking form. Also updating the home page to  have a few links and a bit of content (but more like a place holder)

--- MAPS/forms.py
+++ MAPS/forms.py
@@ -1,30 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, SelectField, TextAreaField
-from wtforms.fields.html5 import DateField, EmailField
+from wtforms import StringField, SubmitField, SelectField, TextAreaField, BooleanField
+from wtforms.fields.html5 import DateField, EmailField, TelField
 from wtforms_components import TimeField
 from wtforms.validators import DataRequired, Length, Email
 
 
 class RegistrationForm(FlaskForm):
     firstname = StringField('First Name',
-                            validators=[DataRequired(), Length(min=2, max=20)])
-    secondname = StringField('Second Name',
-                             validators=[DataRequired(), Length(min=2, max=20)])
+                            validators=[DataRequired(), Length(min=2, max=20)], default="John")
+    secondname = StringField('Second Name', validators=[Length(max=20)])
     lastname = StringField('Last Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    dob = DateField('Date of Birth', format='%Y-%m-%d',
+                           validators=[DataRequired(), Length(min=2, max=20)], default="Smith")
+    dob = DateField('Date of Birth', format='%d/%m/%Y',
                     validators=[DataRequired()])
     gender = SelectField('Please select Gender',
                          choices=[('', 'Please select'), ('m', 'Male'), ('f', 'Female'), ('o', 'other')],
                          validators=[DataRequired()])
     address = TextAreaField('Address',
-                            validators=[DataRequired()])
-    email = EmailField(label="Email", validators=[DataRequired(), Email()])
+                            validators=[DataRequired()], default="2012 Main Street, 3423 Central City ")
+    email = EmailField(label="Email", validators=[DataRequired(), Email()], default="john.smith@hotmail.com")
 
-    phone = IntegerField('Telephone or Mobile',
-                         validators=[DataRequired(), Length(min=1, max=9)])
-    medicare = IntegerField('Medicare No',
-                            validators=[DataRequired(), Length(min=1, max=9)])
+    phone = TelField('Telephone or Mobile',
+                     validators=[DataRequired()])
+    medicare = StringField('Medicare No',
+                           validators=[Length(min=9, max=10, message="Medicare No. has to be 10 digits"),
+                                       DataRequired()], default=1234567890)
     pre_conditions = TextAreaField('Previous Conditions')
     current_medications = TextAreaField('Current Medication')
     pre_doctor = StringField('Previous Doctor')
@@ -33,15 +33,33 @@ class RegistrationForm(FlaskForm):
 
 
 class ConsultationForm(FlaskForm):
-    date =  DateField('Consulation Date', format='%Y-%m-%d',
-                    validators=[DataRequired()])
-    start = TimeField('Start of Consultation',
+    date = DateField('Consulation Date',
+                     validators=[DataRequired()])
+    start = TimeField('Start of Consultation', format='%H:%M',
                       validators=[DataRequired()])
-    end = TimeField('End of Consultation',
+    end = TimeField('End of Consultation', format='%H:%M',
                     validators=[DataRequired()])
     description = TextAreaField('Consultation description',
                                 validators=[DataRequired()])
-    additional_notes = TextAreaField('Current Medication')
+    additional_notes = TextAreaField('Additional Notes')
     symptoms = TextAreaField('Symptoms')
     diagnosis = TextAreaField('Diagnosis')
+
     submit = SubmitField('Save Consultation Notes')
+
+
+class BookingForm(FlaskForm):
+    date = DateField('Consultation Date',
+                     validators=[DataRequired()])
+    start = TimeField('Consulation Time', format='%H:%M',
+                      validators=[DataRequired()])
+    patient_name = StringField('Patient Name',
+                               validators=[DataRequired(), Length(min=2, max=20)], default="John Smith")
+    reason = SelectField('Please select reason for doctors Visit',
+                         choices=[('', 'Please select'), ('1', 'Pick up a prescription'),
+                                  ('2', 'Serious illness - e.g. flu'),
+                                  ('3', 'Medical exam'), ('4', 'Vaccination'), ('5', 'Pick up a medical certificate'),
+                                  ('0', 'unknown')], validators=[DataRequired()])
+    cancelled = BooleanField('Cancelled Appointment')
+
+    submit = SubmitField('Book Consultation')
--- MAPS/routes.py
+++ MAPS/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from MAPS.forms import RegistrationForm, ConsultationForm
+from MAPS.forms import RegistrationForm, ConsultationForm, BookingForm
 from MAPS import app
 
 
@@ -17,10 +17,25 @@ def about():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Your registration was successful!', 'success')
+        return redirect(url_for('home'))
     return render_template('patient_register.html', title='Register', form=form)
 
 
 @app.route("/consultation", methods=['GET', 'POST'])
 def consultation():
     form = ConsultationForm()
+    if form.validate_on_submit():
+        flash('Consultation was successfully saved!', 'success')
+        return redirect(url_for('home'))
     return render_template('consultation.html', title='Consultation', form=form)
+
+
+@app.route("/booking", methods=['GET', 'POST'])
+def booking():
+    form = BookingForm()
+    if form.validate_on_submit():
+        flash('Appointment was successfully created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('booking.html', title='Consultation Booking', form=form)
