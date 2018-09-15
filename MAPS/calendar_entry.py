63ab14600b7049e98ee6862b3e197bc142cffbf5
Pat <patrick.jacob@rocketmail.com>
2018-09-15 10:20:51 +1000

    Just a placeholder document heading

---
+++ MAPS/calendar_entry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+* Authors : Dzmitry Kakaruk, Calvin Schnierer, Patrick Jacob
+*
+* Version info 1.0.
+*
+* This program is created for Assignment 2 of Programming Internet of Things -  Course Master of IT - RMIT University.
+* This code has parts which are inspired by the course material of  - Programming Internet of Things  - RMIT University.
+*
+* The purpose of the Program is to simulate an automated Medical Office..
+* Booking of Consultations, Registration and Recording of Consultations Notes shall be managed via the MAPS Program
+* For more information please see: https://github.com/kokaruk/IOT-A1.
+*
+*
+* Copyright notice - All copyrights belong to  Dzmitry Kakaruk, Calvin Schnierer, Patrick Jacob
+"""
