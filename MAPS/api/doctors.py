77d8eb21952c2bd46e2f1e4f9e1dc2448e70c18a
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-09-16 12:30:21 +1000

    Update doctors api to return all or single calendar id

--- MAPS/api/doctors.py
+++ MAPS/api/doctors.py
@@ -29,3 +29,15 @@ def update_doctor(id):
 @bp.route('/doctors/<int:id>', methods=['DELETE'])
 def delete_doctor(id):
     pass
+
+
+# Get all doctor calendar ids
+@bp.route('/doctors/calendars', methods=['GET'])
+def get_doctors_calendar_ids():
+    pass
+
+
+# Get a doctor's calendar id by id
+@bp.route('/doctors/<int:id>/calendars', methods=['GET'])
+def get_doctor_calendar_id(id):
+    pass
