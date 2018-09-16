faa5f95581f898e1fc383d9ca05b0d74f18b697c
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-09-16 23:32:08 +1000

    FIX added missing line from MAPS __init___

--- MAPS/__init__.py
+++ MAPS/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+
+from MAPS import routes, models
