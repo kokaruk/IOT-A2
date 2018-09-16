93770b71df5863b33fb8b99d739fb938456a2a8d
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-09-16 22:08:00 +1000

    Fixed db, marshmallow and migrate

--- MAPS/__init__.py
+++ MAPS/__init__.py
@@ -4,12 +4,8 @@ from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 
 
-db = SQLAlchemy()
-migrate = Migrate()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
-migrate.init_app(app, db)
-
+migrate = Migrate(app, db)
