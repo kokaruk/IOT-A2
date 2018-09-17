1a152e995e5395b3d28baaa09b38b9c0463a4d4c
rmit-s3668804-Dzmitry-Kakaruk <kokaruk@gmail.com>
2018-09-17 23:08:54 +1000

    push requirements and config

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+    CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+    CLOUDSQL_DATABASE = os.environ.get('CLOUDSQL_DATABASE')
+    CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+    # The CloudSQL proxy is used locally to connect to the cloudsql instance.
+    # To start the proxy, use:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    # Port 3306 is the standard MySQL port. If you need to use a different port,
+    # change the 3306 to a different port number.
+
+    # When running on App Engine a unix socket is used to connect to the cloudsql
+    # instance.
+    SQLALCHEMY_DATABASE_URI = f'mysql://{CLOUDSQL_USER}:{CLOUDSQL_PASSWORD}' \
+                              f'@{CLOUDSQL_CONNECTION_NAME}/{CLOUDSQL_DATABASE}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
