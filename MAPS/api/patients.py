4827426cb3076e3cb90a5b618642479c41faa5c3
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-10-01 13:41:32 +1000

    Patients api docs

--- MAPS/api/patients.py
+++ MAPS/api/patients.py
@@ -31,6 +31,10 @@ referrals_schema = ReferralSchema(many=True)
 # Get all patients
 @bp.route('/patients', methods=['GET'])
 def get_patients():
+    """
+        This route returns all patients in the database
+    :return: JSON array of patients
+    """
     all_patients = Patient.query.all()
     result = part_patients_schema.dump(all_patients)
     return jsonify(result.data)
@@ -39,6 +43,11 @@ def get_patients():
 #  a patient by id
 @bp.route('/patients/<int:id>', methods=['GET'])
 def get_patient(id):
+    """
+        This route returns a single patient matching the id provided. Does not include related data.
+    :param patient id:
+    :return: Patient as a JSON object.
+    """
     patient = Patient.query.get(id)
     result = part_patient_schema.dump(patient)
     return jsonify(result.data)
@@ -47,6 +56,11 @@ def get_patient(id):
 # Get a patient by id - include all related data
 @bp.route('/patients/<int:id>/all', methods=['GET'])
 def get_patient_include_related_data(id):
+    """
+        This route returns a single patient matching the id provided. Includes related data.
+    :param patient id:
+    :return: Patient as a JSON object, with related data.
+    """
     patient = Patient.query.get(id)
     result = full_patient_schema.dump(patient)
     return jsonify(result.data)
@@ -55,6 +69,10 @@ def get_patient_include_related_data(id):
 # Create a patient
 @bp.route('/patients', methods=['POST'])
 def create_patient():
+    """
+        This route accepts a Patient JSON object, and adds to the database.
+    :return: Patient as a JSON object.
+    """
     first_name = request.json['first_name']
     second_name = request.json['second_name']
     last_name = request.json['last_name']
@@ -77,6 +95,10 @@ def create_patient():
 # endpoint to delete patient by id
 @bp.route("/patients", methods=["DELETE"])
 def delete_patient():
+    """
+        This route accepts a json object of the patient's id to delete.
+    :return: Deleted Patient as a JSON object.
+    """
     id = request.json['id']
     patient = Patient.query.get(id)
     db.session.delete(patient)
@@ -89,6 +111,12 @@ def delete_patient():
 # Add a medication for a patient
 @bp.route("/patients/<int:patient_id>/medications", methods=["POST"])
 def add_medication_to_patient(patient_id):
+    """
+        This route accepts a patient id and medication JSON object from request
+        to create a medication for a patient.
+    :param patient_id:
+    :return: Medication JSON object.
+    """
     medication = request.json['medication']
     consultation_details_id = request.json['consultation_details_id']
     new_medication = Medication(patient_id, medication, consultation_details_id)
@@ -100,6 +128,11 @@ def add_medication_to_patient(patient_id):
 # Get all medications for patient by id
 @bp.route("/patients/<int:patient_id>/medications", methods=["GET"])
 def get_all_medication_for_patient(patient_id):
+    """
+        This route accepts a patient id and returns all the patient's medications.
+    :param patient_id:
+    :return: JSON array of patient's medications.
+    """
     all_medications_for_patient = Medication.query.filter(Medication.patient_id == patient_id).all()
     result = medications_schema.dump(all_medications_for_patient)
     return jsonify(result.data)
@@ -108,6 +141,12 @@ def get_all_medication_for_patient(patient_id):
 # endpoint to delete patient by id
 @bp.route("/patients/<int:patient_id>/medications", methods=["DELETE"])
 def delete_medication(patient_id):
+    """
+    This route takes a patient id parameter, and a medication id as a JSON object from the request to delete a
+    medication for a patient.
+    :param patient_id:
+    :return: JSON array of all the patient's medications.
+    """
     id = request.json['id']
     deleted_medication = Medication.query.filter(Medication.id == id).filter(
         Medication.patient_id == patient_id).first()
@@ -123,6 +162,12 @@ def delete_medication(patient_id):
 # Add a condition for a patient
 @bp.route("/patients/<int:patient_id>/conditions", methods=["POST"])
 def add_condition_to_patient(patient_id):
+    """
+        This route takes a patient id, and condition JSON object from the request to add a condition for a
+        patient.
+    :param patient_id:
+    :return: Condition JSON object for patient.
+    """
     condition = request.json['condition']
     consultation_details_id = request.json['consultation_details_id']
     new_condition = Condition(patient_id, condition, consultation_details_id)
@@ -134,6 +179,11 @@ def add_condition_to_patient(patient_id):
 # Get all conditions for patient by id
 @bp.route("/patients/<int:patient_id>/conditions", methods=["GET"])
 def get_all_condition_for_patient(patient_id):
+    """
+        This route takes a patient id to return all the conditions for a patient.
+    :param patient_id:
+    :return: JSON array of conditions for a patient.
+    """
     all_conditions_for_patient = Condition.query.filter(Condition.patient_id == patient_id).all()
     result = conditions_schema.dump(all_conditions_for_patient)
     return jsonify(result.data)
@@ -142,6 +192,12 @@ def get_all_condition_for_patient(patient_id):
 # Delete a condition for a patient by patient id
 @bp.route("/patients/<int:patient_id>/conditions", methods=["DELETE"])
 def delete_condition(patient_id):
+    """
+        This route takes a patient id and condition id from JSON object in request to delete a condition for a
+        patient.
+    :param patient_id:
+    :return: JSON array of all conditions for a patient.
+    """
     id = request.json['id']
     deleted_condition = Condition.query.filter(Condition.id == id).filter(Condition.patient_id == patient_id).first()
     db.session.delete(deleted_condition)
