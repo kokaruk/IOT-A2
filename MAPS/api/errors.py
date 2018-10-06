3a9fcf4eb45f2ddc0886e7ee50b8a6f43e505b80
Calvin Schnierer <calvin.l.schnierer@gmail.com>
2018-10-06 13:41:46 +1000

    Dimi's error message plus flask tutorial api error message

--- MAPS/api/errors.py
+++ MAPS/api/errors.py
@@ -1,2 +1,21 @@
+from flask import jsonify
+from werkzeug.http import HTTP_STATUS_CODES
+from MAPS.api import bp
+
+
+def error_response(status_code, message=None):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    if message:
+        payload['message'] = message
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
+
+
 def bad_request():
-    pass
+    return error_response(400, "An error occurred")
+
+
+@bp.errorhandler(400)
+def custom400(error):
+    return error_response(400, error.description)
